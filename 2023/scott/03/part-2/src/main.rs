33c5086f89b3ea06613e32a04d7900fa421b3794
Scott Callaway <git@scottcallaway.co.uk>
2023-12-05 22:21:24 +0000

    scott day 3 part 2 more changes

--- 2023/scott/03/part-2/src/main.rs
+++ 2023/scott/03/part-2/src/main.rs
@@ -1,35 +1,9 @@
-use std::{fs, intrinsics::raw_eq, time::Instant};
-
-// TODO: Might not need this yet
-enum Symbol {
-    Star,
-    Hash,
-    Plus,
-    Dollar,
-    Slash,
-}
-
-struct PartNumber {
-    value: String,
-    start_index: Option<usize>,
-    is_valid: bool,
-}
-
-impl From<&str> for Symbol {
-    fn from(symbol_text: &str) -> Self {
-        return match symbol_text {
-            "*" => Self::Star,
-            "#" => Self::Hash,
-            "+" => Self::Plus,
-            "$" => Self::Dollar,
-            "/" => Self::Slash,
-            _ => todo!(),
-        };
-    }
-}
+use once_cell::sync::Lazy;
+use regex::{CaptureMatches, Regex};
+use std::{fs, time::Instant};
 
 fn main() {
-    let file = fs::read_to_string("input.txt").expect("Couldn't find file to read");
+    let file = fs::read_to_string("test-input.txt").expect("Couldn't find file to read");
 
     let start = Instant::now();
 
@@ -37,25 +11,32 @@ fn main() {
 
     let all_symbols = file
         .lines()
-        .map(|line| line.chars().collect::<Vec<char>>())
-        .collect::<Vec<Vec<char>>>();
+        .map(|line| line.split("").collect::<Vec<&str>>())
+        .collect::<Vec<Vec<&str>>>();
 
-    let sum: u32 = lines
+    let sum: i32 = lines
         .enumerate()
         .map(|(y_pos, line)| {
-            let mut cog_values: Vec<u32> = Vec::new();
+            let mut cog_values: Vec<i32> = Vec::new();
 
             line.chars().enumerate().for_each(|(x_pos, char)| {
                 if char != '*' {
                     return;
                 }
 
-                let adjacent_numbers = find_adjacent_numbers(x_pos, y_pos, &all_symbols);
+                let adjacent_numbers =
+                    find_surrounding_numbers(x_pos as i32, y_pos as i32, &all_symbols);
+
+                // println!("adjacent_numbers: {:?}", adjacent_numbers);
+
+                if adjacent_numbers.len() != 2 {
+                    return;
+                }
 
                 cog_values.push(adjacent_numbers[0] * adjacent_numbers[1]);
             });
 
-            return cog_values.iter().sum::<u32>();
+            return cog_values.iter().sum::<i32>();
         })
         .sum();
 
@@ -65,62 +46,113 @@ fn main() {
     println!("Took {total_time:?}");
 }
 
-// Verify that there are only ever two numbers surrounding a cog
-fn find_adjacent_numbers(x_pos: usize, y_pos: usize, all_symbols: &Vec<Vec<char>>) -> Vec<u32> {
-    // Top left, top middle, top right
-    let mut top_values: Vec<bool> = Vec::new();
+fn get_line_captures_iter(haystack: &str) -> CaptureMatches {
+    static RE: Lazy<Regex> =
+        Lazy::new(|| Regex::new(r"[0-9]{1,3}").expect("Couldn't generate regex"));
+    RE.captures_iter(haystack)
+}
 
-    // Immediate left and immediate right
-    let mut middle_values: Vec<bool> = Vec::new();
-
-    // Bottom left, bottom middle, bottom right
-    let mut bottom_values: Vec<bool> = Vec::new();
-
-    // Check along the top (if we can)
+fn find_surrounding_numbers(x_pos: i32, y_pos: i32, all_symbols: &Vec<Vec<&str>>) -> Vec<i32> {
+    let mut adjacent_numbers: Vec<i32> = Vec::new();
     if y_pos != 0 {
-        if x_pos != 0 {
-            // Top left
-            top_values.push(all_symbols[y_pos - 1][x_pos - 1].is_digit(10));
-        }
+        // Check all the numbers above the cog
+        get_line_captures_iter(&all_symbols[(y_pos as usize) - 1].join("")).for_each(|capture| {
+            let cap = capture.get(0).expect("Couldn't get first capture");
+            if (cap.start() as i32) < x_pos - 3 {
+                return;
+            }
 
-        // Top middle
-        top_values.push(all_symbols[y_pos - 1][x_pos].is_digit(10));
+            if (cap.end() as i32) > x_pos + 4 {
+                return;
+            }
 
-        if x_pos != all_symbols[0].len() - 1 {
-            // Top right
-            top_values.push(all_symbols[y_pos - 1][x_pos + 1].is_digit(10));
-        }
+            adjacent_numbers.push(
+                cap.as_str()
+                    .parse::<i32>()
+                    .expect("Couldn't parse captured number above"),
+            );
+        });
     }
 
-    // Check the sides (if we can)
+    if y_pos != all_symbols.len() as i32 - 1 as i32 {
+        // Check all the numbers above the cog
+        get_line_captures_iter(&all_symbols[y_pos as usize + 1].join("")).for_each(|capture| {
+            let cap = capture.get(0).expect("Couldn't get first capture");
+            if (cap.start() as i32) < x_pos - 3 {
+                return;
+            }
+
+            if cap.end() as i32 > x_pos + 4 {
+                return;
+            }
+
+            adjacent_numbers.push(
+                cap.as_str()
+                    .parse::<i32>()
+                    .expect("Couldn't parse captured number below"),
+            );
+        });
+    }
+
+    // Check to the left on the same level
     if x_pos != 0 {
-        // Left
-        middle_values.push(all_symbols[y_pos][x_pos - 1].is_digit(10));
-    }
+        // If we can parse, then it's a digit - we should try and capture as
+        // many of these as possible
+        if all_symbols[y_pos as usize][x_pos as usize - 1]
+            .parse::<i32>()
+            .is_ok()
+        {
+            let mut working_value: String = "".to_string();
 
-    if x_pos != all_symbols[0].len() - 1 {
-        // Right
-        middle_values.push(all_symbols[y_pos][x_pos + 1].is_digit(10));
-    }
+            let mut i = x_pos - 1;
+            while all_symbols[y_pos as usize][i as usize]
+                .parse::<i32>()
+                .is_ok()
+            {
+                println!("i: {i}");
+                println!("should be 7: {}", all_symbols[y_pos as usize][i as usize]);
+                // Use insert_str here so that we prepend to the start of the
+                // string to build out the number correctly
+                working_value.insert_str(0, all_symbols[y_pos as usize][i as usize]);
+                i -= 1;
+            }
 
-    // Check the bottom (if we can)
-    if y_pos != all_symbols.len() - 1 {
-        if x_pos != 0 {
-            // Bottom left
-            bottom_values.push(all_symbols[y_pos + 1][x_pos - 1].is_digit(10));
+            println!("working_value: {working_value}");
+
+            adjacent_numbers.push(
+                working_value
+                    .parse::<i32>()
+                    .expect("Couldn't parse captured number to the left"),
+            );
         }
+    }
 
-        // Bottom middle
-        bottom_values.push(all_symbols[y_pos + 1][x_pos].is_digit(10));
+    // Check to the right on the same level
+    if x_pos != all_symbols[0].len() as i32 - 1 {
+        // If we can parse, then it's a digit - we should try and capture as
+        // many of these as possible
+        if all_symbols[y_pos as usize][x_pos as usize + 1]
+            .parse::<i32>()
+            .is_ok()
+        {
+            let mut working_value: String = "".to_string();
 
-        if x_pos != all_symbols[0].len() - 1 {
-            // Bottom right
-            bottom_values.push(all_symbols[y_pos + 1][x_pos + 1].is_digit(10));
+            let mut i = x_pos + 1;
+            while all_symbols[y_pos as usize][i as usize]
+                .parse::<i32>()
+                .is_ok()
+            {
+                working_value.push_str(all_symbols[y_pos as usize][i as usize]);
+                i += 1;
+            }
+
+            adjacent_numbers.push(
+                working_value
+                    .parse::<i32>()
+                    .expect("Couldn't parse captured number to the right"),
+            );
         }
     }
 
-    // We now know whether we have values around us, so we need to ensure we
-    // only have two numbers adjacent
-
-    return Vec::new();
+    return adjacent_numbers;
 }
