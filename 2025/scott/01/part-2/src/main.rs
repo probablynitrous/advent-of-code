1aa8c1746712ea7ff8264c2071a914123fb67452
Scott Callaway <github@scottcallaway.co.uk>
2025-12-01 16:28:28 +0000

    Day 1: part 2 working

--- 2025/scott/01/part-2/src/main.rs
+++ 2025/scott/01/part-2/src/main.rs
@@ -1,7 +1,7 @@
 use std::time::Instant;
 
 fn main() {
-    let file = std::fs::read_to_string("./test-input.txt").expect("Failed to read input file");
+    let file = std::fs::read_to_string("./input.txt").expect("Failed to read input file");
 
     let start = Instant::now();
 
@@ -11,60 +11,34 @@ fn main() {
     file.lines().for_each(|line| {
         let direction = line.chars().nth(0).unwrap();
         let distance = line[1..].parse::<i64>().unwrap();
-        // let full_rotations = distance / 100;
 
-        for i in 0..distance {
-            let new_cursor_position = match direction {
-                'L' => cursor - i,
-                'R' => cursor + i,
-                _ => unimplemented!(),
+        let new_cursor_position = match direction {
+            'L' => cursor - distance,
+            'R' => cursor + distance,
+            _ => unimplemented!(),
+        };
+
+        zero_visits += distance / 100;
+
+        let remainder = distance % 100;
+
+        match direction {
+            'L' => {
+                if cursor - remainder <= 0 && cursor != 0 {
+                    zero_visits += 1;
+                }
             }
-            .rem_euclid(100);
-
-            if new_cursor_position == 0 {
-                zero_visits += 1;
+            'R' => {
+                if cursor + remainder >= 100 {
+                    zero_visits += 1;
+                }
             }
-
-            cursor = new_cursor_position;
+            _ => unimplemented!(),
         }
 
-        // let new_cursor_position = match direction {
-        //     'L' => cursor - distance,
-        //     'R' => cursor + distance,
-        //     _ => unimplemented!(),
-        // };
-        //
-        // if
-        // // If we arrive on a zero
-        // new_cursor_position == 0
-        //     ||
-        // // If we've gone past zero (but didn't start on it)
-        // (new_cursor_position.abs() != new_cursor_position && cursor != 0)
-        // ||
-        // // If we're beyond the limit in either direction
-        // new_cursor_position / 100 > 0
-        // {
-        //     zero_visits += 1;
-        // }
-        //
-        // // Don't need to include the last rotation as we've already captured that in the "landing
-        // // on zero" case
-        // if full_rotations > 0 {
-        //     if new_cursor_position.rem_euclid(100) == 0 {
-        //         zero_visits += full_rotations - 1;
-        //     } else {
-        //         zero_visits += full_rotations;
-        //     }
-        // }
-
-        // println!("cursor: {}", cursor);
-        // println!("instruction: {}{}", direction, distance);
-        // println!("new_cursor_position: {}", new_cursor_position);
-        // println!("zero visit count: {}", zero_visits);
-        // println!("=================");
-
-        // cursor = new_cursor_position.rem_euclid(100);
+        cursor = new_cursor_position.rem_euclid(100);
     });
+
     let total_time = Instant::now() - start;
 
     println!("Final cursor position: {}", cursor);
